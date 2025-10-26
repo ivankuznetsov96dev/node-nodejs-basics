5608da2155355ada4f79ad1092139478b5e6169d
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2025-10-27 00:45:46 +0300

    feat: done compress and decompress tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+import {createReadStream, createWriteStream} from 'node:fs';
+import {createGzip} from 'node:zlib';
+import {pipeline} from 'node:stream/promises';
+import {unlink} from 'node:fs/promises';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const filePath = join(dirName, 'files', 'fileToCompress.txt');
+const archiveFilePath = join(dirName, 'files', 'archive.gz');
+
 const compress = async () => {
-  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream(filePath);
+  const destination = createWriteStream(archiveFilePath);
+
+  try {
+    await pipeline(source, gzip, destination);
+    await unlink(filePath);
+  } catch (error) {}
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+import {createReadStream, createWriteStream} from 'node:fs';
+import {createGunzip} from 'node:zlib';
+import {pipeline} from 'node:stream/promises';
+import {unlink} from 'node:fs/promises';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const filePath = join(dirName, 'files', 'fileToCompress.txt');
+const archiveFilePath = join(dirName, 'files', 'archive.gz');
+
 const decompress = async () => {
-  // Write your code here
+  const gunzip = createGunzip();
+  const source = createReadStream(archiveFilePath);
+  const destination = createWriteStream(filePath);
+
+  try {
+    await pipeline(source, gunzip, destination);
+    await unlink(archiveFilePath);
+  } catch (error) {}
 };
 
 await decompress();
