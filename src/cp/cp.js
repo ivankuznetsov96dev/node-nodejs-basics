86ba90cc94711f7af757ffa28db48aa427776f20
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2025-10-28 19:13:03 +0300

    feat: done cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import {spawn} from 'child_process';
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const scriptPath = join(dirName, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const child = spawn('node', [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  return child;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['test1', 'test2', 'test3', '123']);
